ab96d9e4d2f012d28df308b44af0cc2af20e8e3b
Unknown <denisevbaalen@gmail.com>
2016-12-02 10:00:44 +0100

    Initial files

---
+++ src/nl/ru/ai/exercise3/Exercise3.java
@@ -0,0 +1,80 @@
+package nl.ru.ai.exercise3;
+
+import java.util.ArrayList;
+
+public class Exercise3
+{
+
+  public static void main(String[] args)
+  {
+    /*
+     * Create an ArrayList of names
+     */
+    ArrayList<String> names=new ArrayList<String>();
+    names.add("Sparky");
+    names.add("Nino");
+    names.add("Djamari");
+    names.add("Eireen");
+    names.add("Joan");
+    names.add("Haye");
+    /*
+     * Print them (unsorted)
+     */
+    System.out.println("Ongesorteerd: "+names);
+    /*
+     * Sort them
+     */
+    selectionSort(names);
+    /*
+     * Print them (sorted)
+     */
+    System.out.println("Gesorteerd: "+names);
+  }
+
+  /**
+   * Sorts an array insitu in ascending order using selection sort
+   * @param array
+   * @oaram length
+   */
+  static <T extends Comparable<T>> void selectionSort(ArrayList<T> array)
+  {
+    assert array!=null : "array should be initialized";
+    for(int i=0;i<array.size();i++)
+    {
+      int j=indexOfSmallestValue(array,new Slice(i,array.size()));
+      swap(array,i,j);
+    }
+  }
+  /**
+   * Finds index of smallest value in array slice
+   * @param array
+   * @param slice
+   * @return index of smallest value
+   */
+  static <T extends Comparable<T>> int indexOfSmallestValue(ArrayList<T> array, Slice slice)
+  {
+    assert array!=null : "Array should be initialized";
+    assert slice.isValid()&&slice.upto<=array.size() : "Slice should be valid";
+    assert slice.upto-slice.from>0 : "Slice should be non-empty";
+    int index=slice.from;
+    for(int i=slice.from+1;i<slice.upto;i++)
+      if(array.get(i).compareTo(array.get(index))<0)
+        index=i;
+    return index;
+  }
+  /**
+   * Swap two elements in an array
+   * @param array
+   * @param i
+   * @param j
+   */
+  private static <T extends Comparable<T>> void swap(ArrayList<T> array, int i, int j)
+  {
+    assert array!=null : "Array should be initialized";
+    assert i>=0&&i<array.size() : "First index is invalid";
+    assert j>=0&&j<array.size() : "Second index is invalid";
+    T help=array.get(i);
+    array.set(i,array.get(j));
+    array.set(j,help);
+  }
+}
